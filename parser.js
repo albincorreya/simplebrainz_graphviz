be93b4ae2f357d21beb3a31874efcc1603715c6b
Albin Andrew Correya <albin.a.correya@gmail.com>
2017-08-07 03:36:42 +0200

    Update and rename test.js to parser.js

--- parser.js
+++ parser.js
@@ -1,20 +1,27 @@
 
 
 
-
-
 function responseParser(json){
 	var myJSON = json.results.bindings
 	var myArray = [];
 	for(var i=0; i<myJSON.length; i++){
 		myArray[i] = myJSON[i].o.value;
-		window.alert(myJSON[i].o.value);
+		//window.alert(myJSON[i].o.value);
 	}
 	return myArray;
 }
 
 
+
+
+
+
+
 /*
+
+Some sparql queries for reference
+
+
 select ?s ?p ?o WHERE {
  {<http://simplebrainz.org/album/26b06d23-1910-4d71-a2a2-fdb64ab1fab6#_> ?p ?o}
 UNION
